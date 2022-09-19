8e5735e622c22b21b99217439c6c85cff633f9bf
cjinchi <cjinchi@qq.com>
2022-09-19 20:34:18 +0800

    iterator

--- app.cc
+++ app.cc
@@ -1,5 +1,6 @@
 #include "app.h"
 
+#include <chrono>
 #include <cstring>
 #include <iostream>
 #include <map>
@@ -11,69 +12,40 @@
 
 using namespace std;
 
-class NestedClass {
-   public:
-    NestedClass(const NestedClass& c) {
-        cout << "nested copy constructor" << endl;
-    }
-
-    NestedClass& operator=(const NestedClass& c) {
-        cout << "nested copy assign" << endl;
-    }
-
-    NestedClass(NestedClass&& c) {
-        cout << "nexted move constructor" << endl;
-    }
-
-    NestedClass& operator=(NestedClass&& c) {
-        cout << "nested move assign" << endl;
-    }
-};
-class MyClass {
+template <typename T>
+class Vector {
    private:
-    NestedClass num;
+    T* eles = new T[10];
+    int _size = 0;
 
    public:
-    MyClass(const MyClass& c) : num{c.num} {
-        cout << "class copy constructor" << endl;
+    int size() const {
+        return _size;
     }
 
-    MyClass(MyClass&& a) : num{a.num} {
-        cout << "class move constructor" << endl;
+    T* begin() {
+        return _size ? &eles[0] : nullptr;
     }
 
-    MyClass& operator=(const MyClass& a) {
-        this->num = a.num;
-        cout << "class copy assignment" << endl;
-        return *this;
+    T* end() {
+        return _size ? &eles[0] + _size : nullptr;
     }
 
-    MyClass& operator=(MyClass&& a) {
-        num = a.num;
-        // a.num = 0;
-        cout << "class move assignment" << endl;
-        return *this;
+    void push_back(const T& val) {
+        eles[_size++] = val;
     }
-
-    // MyClass operator+(const MyClass& a) {
-    //     cout << "operator +" << endl;
-    //     MyClass ret(num + a.num);
-    //     return ret;
-    // }
-
-    // void print() {
-    //     cout << num << endl;
-    // }
 };
 
-MyClass get_my_class() {
-    MyClass ret;
-    return ret;
-}
-
-// should be ran with -fno-elide-constructors flag
 int main() {
-    MyClass a = get_my_class();
-    // a.print();
+    Vector<string> vecs;
+    vecs.push_back("hello"s);
+    vecs.push_back("world"s);
+    for (auto itr = vecs.begin(); itr != vecs.end(); itr++) {
+        cout << *itr << endl;
+    }
+    for (auto& v : vecs) {
+        cout << v << endl;
+    }
+
     return 0;
 }
