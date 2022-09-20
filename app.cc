49c24e9f158bf8744ee92ef0f4ad8bdcd2a5a556
cjinchi <cjinchi@qq.com>
2022-09-20 10:42:52 +0800

    Revert "function object"
    This reverts commit d3c96257adb28b2c4e368c65436786cf0974d90a.

--- app.cc
+++ app.cc
@@ -12,12 +12,23 @@
 
 using namespace std;
 
-template<typename T>
-void print(const T& v) {
-    cout << v << endl;
-}
+template <typename T>
+class Less_than {
+    const T val;
+
+   public:
+    Less_than(const T& v) : val{v} {}
+    bool operator()(const T& x) const {
+        return x < val;
+    }
+};
 
 int main() {
-    print("hello world"s);
+    Less_than lti{100};
+    cout << lti(150) << endl;
+    Less_than lts{"b"s};
+    cout << lts("abc"s) << endl;
+    cout << lts("c"s) << endl;
+
     return 0;
 }
